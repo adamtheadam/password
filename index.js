d89897623e369c17988808623db8522692537ba2
Adam Rös <adaros0128@skynet.halmstad.se>
2019-11-14 12:52:24 +0100

    lektion2

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+
+const app = express();
+
+app.use(express.urlencoded({extended:false}));
+
+app.get("/",function(req,res){
+    res.send("index route...");
+});
+
+app.get("/login",function(req,res){
+    res.sendFile(__dirname+"/loginform.html");
+});
+
+app.post("/login",function(req,res){
+
+   // Hämta våra användare från db/fil
+   const users = require("./users");
+
+   const user = users.filter(function(u){
+
+    return req.body.email === u.email;
+   
+
+   });
+
+   if(user.length===1)
+   {
+
+    // kolla lösenord
+    bcrypt.compare(req.body.password,user[0].password,function(err,success){
+
+        if(success){
+            res.send("Login success");
+        }
+        else{
+            res.send("Wrong password");
+        }
+
+    });
+    
+   }
+   else
+   {
+    res.send("No such user");
+   }
+
+
+
+    /**
+     * 1. hämta data som klienten skickat ( Repetition )
+     * 2. Leta efter användare i databas/fil/minne
+     * 3. Om användare ej finns skicka respons till klient med error
+     * 4. Om användare finns gå vidare med att kolla lösenord
+     * 5. Om löserord ej är korrekt skicka respons till klient med error
+     * 6. Om lösenord är korrekt - Skicka respons/redirect 
+     * 7. Nu när användaren är inloggad måste hen förbli så ett ta
+     *    Detta löser vi med JWT.
+     *    Skapa JWT och lagra i cookie innan din respons/redirect
+     * 8. Skapa middleware för att skydda vissa routes.
+     *    Här skall vi nu använda våra JWT för att hålla en användare inloggad. 
+     * 9. Småfix för att förbättra säkerhet och fixa utloggning. 
+     */
+
+  
+
+});
+
+// kollar om systemet har en angiven port, annars 3700...
+const port = process.env.PORT || 3700
+app.listen(port, function(){console.log("port:" +port)});
---
+++ users.js
@@ -0,0 +1,6 @@
+
+module.exports = [
+    {id:1, email:"lars@lasse.se", password:"$2a$12$ht0kDMKQu7yTrDmIAaOJ3O.ne59gTc9k03MWWrxHQeX/I84J136HK"},
+    {id:2, email:"lena@larsson.se", password:"$2a$12$zXpuOO0jMDnzXTrPKBi/ruUlxtu4U/13f.lG4DzWXlNrL4FQqS1CW"},
+    {id:3, email:"jens@peter.se", password:"$2a$12$YDUhkYdcMvCX8JiUUn1k8.e6jQJRHFeUmTXnkMMYfK3l44MJ5/dy."}
+];
